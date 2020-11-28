856c1a6144a99e85006a67ab3a986b318b09b818
Solokhin <marugo@mail.ru>
2020-11-28 20:14:53 +0300

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,11 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 3000,
   });
+
+  $('.star').rateYo({
+    starWidth: '17px',
+    normalFill: '#ccccce',
+    rateFill: '#ffc35b',
+    readOnly: true,
+  });
 });
